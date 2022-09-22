4d0faf6f41d897db3800edea70f803cfb940e95b
emsweetland <emmaksweetland@gmail.com>
2022-09-22 14:56:01 -0500

    getpets

---
+++ Migrations/20220922195117_NotMapped.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class NotMapped : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "checkedInAt",
+                table: "Pets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Pets",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Pets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "checkedInAt",
+                table: "Pets",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,34 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType {
+        Cat,
+        Dog,
+        Snake,
+        Rat
+    }
+    public enum PetColorType {
+        Black,
+        White,
+        Pink,
+        Orange
+    }
+    public class Pet {
+        public int id {get; set;}
+        [Required]
+        public string name {get; set;}
+        [JsonConverter (typeof(JsonStringEnumConverter))]
+        [Required]
+        public PetBreedType breed {get; set;}
+        [Required]
+        public PetColorType color {get; set;}
+        [NotMapped]
+        public DateTime checkedInAt {get; set;}
+
+[ForeignKey("petOwner")]
+    [Required]
+    public int petOwnerid {get; set;}
+    
+    public PetOwner petOwner {get; set;}
+    }
 }
